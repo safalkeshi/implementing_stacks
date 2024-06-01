ebe8362a9d9aaf22c1adea1b7ffad442d77b95f1
Safal K.c. <exito11akasucess@gmail.com>
2024-06-01 17:13:27 +0545

    another day

--- input.js
+++ input.js
@@ -2,4 +2,13 @@ const icons=document.getElementsByClassName('seat');
 const park=document.getElementsByClassName('Parked');
 const empty=document.getElementsByClassName('Empty');
 const select=document.getElementsByClassName('Selected');
-console.log(park,empty,select);
+const entrybtn=document.querySelector("Entry");
+const exitbtn=document.querySelector("Exit");
+ const changeprop=document.getElementsByClassName('entry');
+
+
+var display =0;
+function hideshow(){
+    if(display==1){
+        
+}
